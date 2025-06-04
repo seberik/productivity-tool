f7ef940836ac9ca1c8f7c503da6f4702cfcba7d3
Sebastian Eriksson <sebastian.v.eriksson@gmail.com>
2025-06-04 22:30:19 +0200

    chore: allow images to be served from images.svc.ui.com

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.svc.ui.com',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
