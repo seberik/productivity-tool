1f2b4f47c8a53dbea06ae3e56ca33baa392407f3
Sebastian Eriksson <sebastian.v.eriksson@gmail.com>
2025-06-08 23:37:08 +0200

    feat: add support for passing classname

--- components/common/DeviceImage/DeviceImage.tsx
+++ components/common/DeviceImage/DeviceImage.tsx
@@ -5,19 +5,23 @@ interface DeviceImageProps {
   width: number;
   height: number;
   device: Pick<Device, "id" | "name" | "images">;
+  className?: HTMLImageElement['className'],
 }
 
 export function DeviceImage({
   width,
   height,
   device,
+  className,
 }: DeviceImageProps) {
+
   return (
     <Image
+      className={className}
       width={width}
       height={height}
       src={`https://images.svc.ui.com/?u=https%3A%2F%2Fstatic.ui.com%2Ffingerprint%2Fui%2Fimage
-  s%2F${device.id}%2Fdefault%2F${device.images.default}.png&w=${width}&q=${height}`}
+s%2F${device.id}%2Fdefault%2F${device.images.default}.png&w=${width}&q=75`}
       alt={device.name}
       loading="lazy"
     ></Image>
